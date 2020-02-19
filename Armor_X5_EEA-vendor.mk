f7fb7877f6916d9248d1946d999579c1976873d7
Kirill <vsegda_online_007@bk.ru>
2020-02-19 15:43:36 +0300

    Ulefone Armor X5: import ST NFC stack

--- Armor_X5_EEA-vendor.mk
+++ Armor_X5_EEA-vendor.mk
@@ -23,7 +23,26 @@ PRODUCT_COPY_FILES += \
     vendor/Ulefone/Armor_X5_EEA/proprietary/etc/init/kpoc_charger.rc:system/etc/init/kpoc_charger.rc \
     vendor/Ulefone/Armor_X5_EEA/proprietary/lib/libshowlogo.so:system/lib/libshowlogo.so \
     vendor/Ulefone/Armor_X5_EEA/proprietary/lib/libsysenv_system.so:system/lib/libsysenv_system.so \
-    vendor/Ulefone/Armor_X5_EEA/proprietary/lib64/libmtk-ril.so:system/lib64/libmtk-ril.so
+    vendor/Ulefone/Armor_X5_EEA/proprietary/lib64/libmtk-ril.so:system/lib64/libmtk-ril.so \
+    vendor/umidigi/F1/proprietary/bin/nfcstackp:$(TARGET_COPY_OUT_SYSTEM)/bin/nfcstackp \
+    vendor/umidigi/F1/proprietary/etc/nfcee_access.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/nfcee_access.xml \
+    vendor/umidigi/F1/proprietary/etc/permissions/com.st.android.nfc_dta_extensions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.st.android.nfc_dta_extensions.xml \
+    vendor/umidigi/F1/proprietary/etc/permissions/com.st.android.nfc_extensions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.st.android.nfc_extensions.xml \
+    vendor/umidigi/F1/proprietary/lib/libdtaNfaProvider.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libdtaNfaProvider.so \
+    vendor/umidigi/F1/proprietary/lib/libicuXD.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libicuXD.so \
+    vendor/umidigi/F1/proprietary/lib/libnfc_st_dta.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libnfc_st_dta.so \
+    vendor/umidigi/F1/proprietary/lib/libnfc_st_dta_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libnfc_st_dta_jni.so \
+    vendor/umidigi/F1/proprietary/lib/libstnfc-nci.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libstnfc-nci.so \
+    vendor/umidigi/F1/proprietary/lib/libstnfc_nci_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libstnfc_nci_jni.so \
+    vendor/umidigi/F1/proprietary/lib64/libdtaNfaProvider.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libdtaNfaProvider.so \
+    vendor/umidigi/F1/proprietary/lib64/libicuXD.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libicuXD.so \
+    vendor/umidigi/F1/proprietary/lib64/libnfc_st_dta.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libnfc_st_dta.so \
+    vendor/umidigi/F1/proprietary/lib64/libnfc_st_dta_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libnfc_st_dta_jni.so \
+    vendor/umidigi/F1/proprietary/lib64/libstnfc-nci.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libstnfc-nci.so \
+    vendor/umidigi/F1/proprietary/lib64/libstnfc_nci_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libstnfc_nci_jni.so
     	
 PRODUCT_PACKAGES += \
-    FCamera
+    FCamera \
+    Nfc_st \
+    com.st.android.nfc_dta_extensions \
+    com.st.android.nfc_extensions
